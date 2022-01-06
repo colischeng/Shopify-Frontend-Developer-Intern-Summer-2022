e359ac794ee83ddbfbed3b6bd8d10c8597675cb5
Colis Cheng <colis_cheng@thecdm.ca>
2022-01-06 15:56:00 -0800

    Redux template with simple withdraw/deposit example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state/index";
 
 const App = () => {
+  const account = useSelector((state) => state.account);
+  const dispatch = useDispatch();
+
+  const { withdraw, deposit } = bindActionCreators(actionCreators, dispatch);
+
   return (
     <div>
-      <h1>App</h1>
+      <h1>{account}</h1>
+      <button onClick={() => deposit(1000)}>Deposit</button>
+      <button onClick={() => withdraw(1000)}>Withdraw</button>
     </div>
   );
 };

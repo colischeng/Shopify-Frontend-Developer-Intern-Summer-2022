d064797afc9273fb0292cb503f445673892fd0ac
Colis Cheng <colis_cheng@thecdm.ca>
2022-01-07 19:05:07 -0800

    Conditional render posts based on toggle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import { Grid } from "@mui/material";
 import { Banner } from "./components/Banner/Banner";
 import { Toggle } from "./components/Toggle/Toggle";
 import { QueryPosts } from "./components/QueryPosts/QueryPosts";
 
 const App = () => {
+  const [view, setView] = useState(true);
+  // true means we are viewing queried posts
+  // false means we are viewing liked posts
+
+  const handleSwitch = () => {
+    setView(!view);
+  };
+
   return (
     <Grid>
       <Banner />
-      <Toggle />
-      <QueryPosts />
+      <Toggle handleSwitch={handleSwitch} />
+      {view ? <QueryPosts /> : <div> hello</div>}
     </Grid>
   );
 };
--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -1,7 +1,7 @@
 import { Grid, Stack, Switch, Typography } from "@mui/material";
 import Offset from "../Offset";
 
-export const Toggle = () => {
+export const Toggle = (props) => {
   return (
     <Grid
       container
@@ -12,7 +12,11 @@ export const Toggle = () => {
     >
       <Stack direction="row" spacing={1} alignItems="center">
         <Typography variant="h5">View Liked Posts</Typography>
-        <Switch defaultChecked inputProps={{ "aria-label": "ant design" }} />
+        <Switch
+          defaultChecked
+          inputProps={{ "aria-label": "ant design" }}
+          onChange={props.handleSwitch}
+        />
         <Typography variant="h5">Query Posts</Typography>
       </Stack>
       <Offset />
